4e63dfa4294486a3aa0af5a3b480514dd3454982
afrikanov <barcelona1405@mail.ru>
2019-03-13 16:52:22 +0300

    changed task1.1

---
+++ hw01/task1/src/task1/hw1/afrikanov/Stack.java
@@ -0,0 +1,50 @@
+package task1.hw1.afrikanov;
+
+/** * Class implements methods of Stack. */
+public class Stack {
+
+    private Node head = new Node(0, null);
+    private int size = 0;
+
+    /**
+     * Method adds an element to the end.
+     *
+     * @param value
+     */
+    public void push(int value) {
+        head = new Node(value, head);
+        size++;
+    }
+
+    /** * Method erases an element from the end. */
+    public void pop() {
+        if (empty()) {
+            return;
+        }
+        head = head.next;
+        size--;
+    }
+
+    public int top() {
+        return head.value;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean empty() {
+        return size == 0;
+    }
+
+    /** * Class implements structure with 2 fields : value and link on the next element. */
+    private class Node {
+        private int value;
+        private Node next;
+
+        Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+}
