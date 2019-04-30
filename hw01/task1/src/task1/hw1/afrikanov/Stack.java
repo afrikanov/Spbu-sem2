0931ff9515a33be766f6641ab9f43c704a20a4d2
afrikanov <barcelona1405@mail.ru>
2019-04-30 18:34:13 +0300

    changed task1.1

--- hw01/task1/src/task1/hw1/afrikanov/Stack.java
+++ hw01/task1/src/task1/hw1/afrikanov/Stack.java
@@ -1,51 +1,53 @@
 package task1.hw1.afrikanov;
 
 /** * Class implements methods of Stack. */
-public class Stack {
+public class Stack<Type> {
 
-    private Node head = new Node(0, null);
+    private Node head = null;
     private int size = 0;
 
     /**
      * Method adds an element to the end.
      * @param value - value of a new element
      */
-    public void push(int value) {
+    public void push(Type value) {
         head = new Node(value, head);
         size++;
     }
 
-    /** * Method erases an element from the end. */
-    public void pop() {
-        if (empty()) {
-            return;
+    /** Method erases an element from the end. */
+    public Type pop() {
+        if (isEmpty()) {
+            return null;
         }
+        Type previousTop = head.value;
         head = head.next;
         size--;
+        return previousTop;
     }
 
     /**
      * Method gets an element from the top of stack
      * @return value from the top
      */
-    public int top() {
-        return head.value;
+    public Type top() {
+        return head == null ? null : head.value;
     }
 
-    public int size() {
+    public int getSize() {
         return size;
     }
 
-    public boolean empty() {
+    public boolean isEmpty() {
         return size == 0;
     }
 
-    /** * Class implements structure with 2 fields : value and link on the next element. */
+    /** Class implements structure with 2 fields : value and link on the next element. */
     private class Node {
-        private int value;
+        private Type value;
         private Node next;
 
-        Node(int value, Node next) {
+        Node(Type value, Node next) {
             this.value = value;
             this.next = next;
         }
