4bf1a14df92b5e5b003957c0bdd31c88292bc3b0
afrikanov <barcelona1405@mail.ru>
2019-05-07 04:30:39 +0300

    fixed task04.2

--- hw04/task2/src/main/java/group144/afrikanov/NoNextElementException.java
+++ hw04/task2/src/main/java/group144/afrikanov/NoNextElementException.java
@@ -1,5 +1,6 @@
 package group144.afrikanov;
 
+/** Class realizes an exception that throws when next element after a certain does not exist */
 public class NoNextElementException extends RuntimeException {
     public NoNextElementException(String exceptionText) {
         super(exceptionText);
