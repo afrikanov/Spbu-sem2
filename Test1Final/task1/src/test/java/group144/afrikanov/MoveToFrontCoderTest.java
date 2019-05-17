65096cfb8c86b838447f3e5b54fe690f4658508f
afrikanov <barcelona1405@mail.ru>
2019-05-17 16:58:33 +0300

    added new task1 solution

---
+++ Test1Final/task1/src/test/java/group144/afrikanov/MoveToFrontCoderTest.java
@@ -0,0 +1,28 @@
+package group144.afrikanov;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MoveToFrontCoderTest {
+
+    @Test
+    public void ExampleCodeTest() {
+        MoveToFrontCoder coder = new MoveToFrontCoder();
+        String word = "banana";
+        List<Integer> expected = new ArrayList<>(Arrays.asList(1, 1, 13, 1, 1, 1));
+        assertEquals(expected, coder.code(word));
+    }
+
+    @Test
+    public void exampleEncodeTest() {
+        MoveToFrontCoder encoder = new MoveToFrontCoder();
+        List<Integer> list = new ArrayList<>(Arrays.asList(1, 1, 13, 1, 1, 1));
+        String expected = "banana";
+        assertEquals(expected, encoder.encode(list));
+    }
+}
